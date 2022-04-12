593784e253b41dfd3eb13e7eee60a3b296533c30
shenmingyuan <shenmingyuan@meituan.com>
2022-04-12 14:35:16 +0800

    Add git info to prompt

--- configs.sh
+++ configs.sh
@@ -1,5 +1,10 @@
-export PROMPT='%{$GREEN_BOLD%}%n@%m%{$WHITE%}:%{$YELLOW%}%~%u%{$RESET_COLOR%}
+export PROMPT='%{$GREEN_BOLD%}%n@%m%{$WHITE%}:%{$YELLOW%}%~%u%{$RESET_COLOR%} $(git_prompt_info)
 %{$BLUE%}>%{$RESET_COLOR%} '
+
+ZSH_THEME_GIT_PROMPT_PREFIX="%{$fg_bold[blue]%}git:(%{$fg[red]%}"
+ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%} "
+ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg[blue]%}) %{$fg[yellow]%}✗"
+ZSH_THEME_GIT_PROMPT_CLEAN="%{$fg[blue]%})"
 
 unset RPROMPT
 
